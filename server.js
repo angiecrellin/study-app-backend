a924a713d91945224b72d43690ae66550af2d2d6
angiecrellin <angiecrellin@gmail.com>
2016-12-19 11:47:03 -0600

    v3

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ mongoose.connect('mongodb://angiecrellin:Cal92683@ds127998.mlab.com:27998/flashc
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const port = 8080
 
 app.post('/create-card', (req, res) => {
